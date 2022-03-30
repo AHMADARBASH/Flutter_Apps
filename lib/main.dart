dfb56496815a6a4aed23165ac8b1918446435d10
AHMADARBASH <ahmad.arbash.97@outlook.com>
2022-03-30 13:09:56 +0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,329 +1,124 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'dart:math';
 
-// ignore: curly_braces_in_flow_control_structures
-void main() {
-  runApp(MyApp());
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-Color firstcolor = const Color.fromARGB(255, 73, 84, 100);
-Color secondcolor = const Color.fromARGB(100, 187, 191, 202);
-Color thirdcolor = const Color.fromARGB(255, 232, 232, 232);
-Color fourthcolor = const Color.fromARGB(255, 244, 244, 244);
-
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Clock(),
+      home: MyClock(),
     );
   }
 }
 
-class Clock extends StatelessWidget {
-  const Clock({
-    Key? key,
-  }) : super(key: key);
+class MyClock extends StatelessWidget {
+  const MyClock({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: fourthcolor,
-        appBar: AppBar(
-          title: const Center(child: Text('Flutter Clock')),
-          backgroundColor: firstcolor,
-        ),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Flutter Test')),
         body: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Expanded(
-                flex: 2,
-                child: Container(
-                  alignment: AlignmentDirectional.center,
-                  child: Stack(
-                    alignment: AlignmentDirectional.center,
-                    children: [
-                      //Hours Angle
-                      StreamBuilder<dynamic>(
-                          initialData: const {
-                            'hours': 0,
-                            'minutes': 0,
-                            'seconds': 0,
-                            'mesconds': 0,
-                          },
-                          stream: seqAsyncValues(),
-                          builder: (context, data) {
-                            {
-                              return Transform.rotate(
-                                angle: ((data.data['hours'] - 12) * 60 +
-                                        data.data['minutes']) *
-                                    ((pi / 180) * 0.5),
-                                child: Column(
-                                  children: [
-                                    Expanded(
-                                        flex: 2,
-                                        child: Container(
-                                          width: (MediaQuery.of(context)
-                                                      .size
-                                                      .height /
-                                                  100) *
-                                              1,
-                                        )),
-                                    Expanded(
-                                        flex: 2,
-                                        child: Container(
-                                          color: firstcolor,
-                                          width: (MediaQuery.of(context)
-                                                      .size
-                                                      .height /
-                                                  100) *
-                                              2,
-                                        )),
-                                    Expanded(
-                                      flex: 2,
-                                      child: Container(
-                                          width: (MediaQuery.of(context)
-                                                      .size
-                                                      .height /
-                                                  100) *
-                                              1),
-                                    ),
-                                    Expanded(
-                                        flex: 2,
-                                        child: Container(
-                                          width: (MediaQuery.of(context)
-                                                      .size
-                                                      .height /
-                                                  100) *
-                                              1,
-                                        ))
-                                  ],
-                                ),
-                              );
-                            }
-                          }), //end of hours angle
-
-                      //Minutes Angle
-                      StreamBuilder<dynamic>(
-                          stream: seqAsyncValues(),
-                          builder: (context, data) {
-                            if (!data.hasData) {
-                              return const CircularProgressIndicator();
-                            } else {
-                              return Transform.rotate(
-                                angle: (((data.data!['minutes']) * 60 +
-                                        data.data['seconds']) *
-                                    ((pi / 180) * 0.1)),
-                                child: Column(
-                                  children: [
-                                    Expanded(
-                                        flex: 2,
-                                        child: Container(
-                                          width: (MediaQuery.of(context)
-                                                      .size
-                                                      .height /
-                                                  100) *
-                                              1,
-                                        )),
-                                    Expanded(
-                                        flex: 3,
-                                        child: Container(
-                                          color: firstcolor,
-                                          width: (MediaQuery.of(context)
-                                                      .size
-                                                      .height /
-                                                  100) *
-                                              1.5,
-                                        )),
-                                    Expanded(
-                                      flex: 3,
-                                      child: Container(
-                                          width: (MediaQuery.of(context)
-                                                      .size
-                                                      .height /
-                                                  100) *
-                                              1),
-                                    ),
-                                    Expanded(
-                                        flex: 2,
-                                        child: Container(
-                                          width: (MediaQuery.of(context)
-                                                      .size
-                                                      .height /
-                                                  100) *
-                                              1,
-                                        ))
-                                  ],
-                                ),
-                              );
-                            }
-                          }), //end of Minutes angle
-                      //seconds angle
-                      StreamBuilder<dynamic>(
-                          stream: seqAsyncValues(),
-                          builder: (context, data) {
-                            if (!data.hasData) {
-                              return const CircularProgressIndicator();
-                            } else {
-                              return Transform.rotate(
-                                angle: ((data.data['seconds'] * 1000 +
-                                        data.data['mesconds']) *
-                                    ((pi / 180) * 0.006)),
-                                child: Column(
-                                  children: [
-                                    Expanded(
-                                        flex: 2,
-                                        child: Container(
-                                          width: (MediaQuery.of(context)
-                                                      .size
-                                                      .height /
-                                                  100) *
-                                              1,
-                                        )),
-                                    Expanded(
-                                      flex: 4,
-                                      child: Container(
-                                        color: Colors.red,
-                                        width:
-                                            MediaQuery.of(context).size.height *
-                                                0.01,
-                                      ),
-                                    ),
-                                    Expanded(
-                                      flex: 4,
-                                      child: Container(
-                                        width:
-                                            MediaQuery.of(context).size.height *
-                                                0.01,
-                                      ),
-                                    ),
-                                    Expanded(
-                                        flex: 2,
-                                        child: Container(
-                                          width: MediaQuery.of(context)
-                                                  .size
-                                                  .height *
-                                              0.01,
-                                        ))
-                                  ],
-                                ),
-                              );
-                            }
-                          }), //end of seconds angle
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.1,
-                        height: MediaQuery.of(context).size.width * 0.1,
-                        decoration: BoxDecoration(
-                          color: firstcolor,
-                          shape: BoxShape.circle,
-                        ),
-                      ),
-
-                      Numbers(),
-                      Transform.rotate(
-                        angle: (pi / 180) * 30,
-                        child: Numbers(),
-                      ),
-                      Transform.rotate(
-                        angle: (pi / 180) * 60,
-                        child: Numbers(),
-                      ),
-                      Transform.rotate(
-                        angle: (pi / 180) * 90,
-                        child: Numbers(),
-                      ),
-                      Transform.rotate(
-                        angle: (pi / 180) * 120,
-                        child: Numbers(),
-                      ),
-                      Transform.rotate(
-                        angle: (pi / 180) * 150,
-                        child: Numbers(),
-                      )
-                    ],
-                  ),
-                  decoration:
-                      BoxDecoration(color: secondcolor, shape: BoxShape.circle),
+              ElevatedButton(
+                child: const Text(
+                  'show alert dialog',
+                  style: TextStyle(fontSize: 22),
                 ),
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (_) {
+                        return AlertDialog(
+                          title: const Text('Alert dialog'),
+                          actions: [
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('Yes')),
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('No')),
+                          ],
+                        );
+                      });
+                },
               ),
-              //digital clock
-              Expanded(
-                flex: 1,
-                child: StreamBuilder(
-                  stream: seqAsyncValues(),
-                  builder: (ctx, data) {
-                    if (!data.hasData)
-                      // ignore: curly_braces_in_flow_control_structures
-                      return const CircularProgressIndicator();
-                    else {
-                      return (Center(
-                        child: Text(
-                            DateFormat('hh:mm:ss  a').format(DateTime.now()),
-                            style: TextStyle(
-                              fontSize: 40,
-                              color: firstcolor,
-                            )),
-                      ));
-                    }
-                  },
+              SizedBox(
+                height: 40,
+              ),
+              ElevatedButton(
+                child: const Text(
+                  'show modal bottom sheet',
+                  style: TextStyle(fontSize: 22),
                 ),
-              )
+                onPressed: () {
+                  showModalBottomSheet(
+                      context: context,
+                      builder: (context) {
+                        return const Center(
+                          child: Padding(
+                            padding: EdgeInsets.all(20.0),
+                            child: Text(
+                              'This is a modal buttom sheet',
+                              style: TextStyle(fontSize: 30),
+                            ),
+                          ),
+                        );
+                      });
+                },
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              ElevatedButton(
+                child: const Text(
+                  'Combine both together',
+                  style: TextStyle(fontSize: 22),
+                ),
+                onPressed: () {
+                  showModalBottomSheet(
+                      isScrollControlled: true,
+                      backgroundColor: Colors.transparent,
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: const Text('Alert dialog'),
+                          actions: [
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('Yes')),
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('No')),
+                          ],
+                        );
+                      });
+                },
+              ),
             ],
           ),
-        ));
-  }
-}
-
-class Numbers extends StatelessWidget {
-  const Numbers({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        SizedBox(
-            child: Center(
-              child: Container(
-                  //side black lines
-                  color: Colors.black,
-                  width: MediaQuery.of(context).size.width * 0.1,
-                  height: MediaQuery.of(context).size.height * 0.01),
-            ),
-            width: MediaQuery.of(context).size.width * 0.1,
-            height: MediaQuery.of(context).size.height * 0.5),
-        SizedBox(
-            width: MediaQuery.of(context).size.width * 0.8,
-            height: MediaQuery.of(context).size.height * 0.5),
-        SizedBox(
-            child: Center(
-              child: Container(
-                color: Colors.black,
-                width: MediaQuery.of(context).size.width * 0.1,
-                height: MediaQuery.of(context).size.height * 0.01,
-              ),
-            ),
-            width: MediaQuery.of(context).size.width * 0.1,
-            height: MediaQuery.of(context).size.height * 0.5),
-      ],
+        ),
+      ),
     );
-  }
-}
-
-Stream<Map<String, int>> seqAsyncValues() async* {
-  for (var i = 0;; i++) {
-    DateTime date = DateTime.now();
-    await Future.delayed(const Duration(milliseconds: 10));
-    Map<String, int> dateMap = {
-      'hours': date.hour,
-      'minutes': date.minute,
-      'seconds': date.second,
-      'mesconds': date.millisecond,
-    };
-
-    yield dateMap;
   }
 }
